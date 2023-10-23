f20cb55703095797386f5c9b21b76b80e58903bd
김민정 <dst04021@gmail.com>
2023-10-23 12:59:04 +0900

    feat: 오류 수정 및 전체 적용

--- js/script.js
+++ js/script.js
@@ -1,145 +1,211 @@
-// 메뉴 기능
-const nav = document.querySelector(".nav");
-const btMenu = document.querySelector(".bt-menu");
-const navClose = document.querySelector(".nav-close");
-
-// 펼치기
-btMenu.addEventListener("click", function(){
-    // 클래스를 nav에 추가.
-    nav.classList.add("nav-active");
-});
-// 닫기
-navClose.addEventListener("click", function(){
-    nav.classList.remove("nav-active");
-});
-// nav를 벗어나도 없어지게 함.
-nav.addEventListener("mouseleave", function(){
-    nav.classList.remove("nav-active");
-});
-
-
-// 스크롤 기능
-// 스크롤바의 상단위치
-let scy = 0;
-let scActive = 50;
-scy = window.document.documentElement.scrollTop;
-let header = document.querySelector(".header");
-let logoW = document.querySelector(".logo-w");
-let logoG = document.querySelector(".logo-g");
-
-header.addEventListener("mouseenter", function(){
-    header.classList.add("header-active");
-    logoW.style.display = "none";
-    logoG.style.display = "block";
-});
-header.addEventListener("mouseleave", function(){
-    if(scy < scActive){
+window.onload = function () {
+    // AOS적용
+    AOS.init();
+    // 안내창
+    let body = document.querySelector("body");
+    let modal = document.querySelector(".modal-wrap");
+    modal.addEventListener("click", function () {
+      // modal.style.display = "none";
+      // fadeOut(modal);
+      anime({
+        targets: ".modal",
+        delay: 200,
+        duration: 500,
+        opacity: 0,
+        easing: "easeInOutQuad",
+        complete: function () {
+          modal.style.display = "none";
+          body.classList.add("active");
+        },
+      });
+    });
+    // 메뉴기능
+    const nav = document.querySelector(".nav");
+    const btMenu = document.querySelector(".bt-menu");
+    const navClose = document.querySelector(".nav-close");
+    btMenu.addEventListener("click", function () {
+      //   클래스를 nav에 추가하고 싶다.
+      nav.classList.add("nav-active");
+    });
+    navClose.addEventListener("click", function () {
+      // 클래스를 nav에 삭제하고 싶다.
+      nav.classList.remove("nav-active");
+    });
+    // nav 영역을 벗어나느 이벤트 발생처리
+    nav.addEventListener("mouseleave", function () {
+      nav.classList.remove("nav-active");
+    });
+    // 스크롤 기능
+    // 스크롤바의 상단위치
+    let scy = 0;
+    let scActive = 50;
+    scy = window.document.documentElement.scrollTop;
+    let header = document.querySelector(".header");
+    let logoW = document.querySelector(".logo-w");
+    let logoG = document.querySelector(".logo-g");
+    header.addEventListener("mouseenter", function () {
+      header.classList.add("header-active");
+      logoW.style.display = "none";
+      logoG.style.display = "block";
+    });
+    header.addEventListener("mouseleave", function () {
+      if (scy < scActive) {
         header.classList.remove("header-active");
         logoW.style.display = "block";
         logoG.style.display = "none";
+      }
+    });
+    //새로고침 시 적용
+    if (scy > scActive) {
+      header.classList.add("header-active");
+      logoW.style.display = "none";
+      logoG.style.display = "block";
     }
-});
-
-// 새로고침 시 스크롤이 그대로일때 헤더도 그대로 있도록 적용.
-if (scy > scActive) {
-    header.classList.add("header-active");
-    logoW.style.display = "none";
-    logoG.style.display = "block";
-};
-
-window.addEventListener("scroll", function(){
-    scy = window.document.documentElement.scrollTop;
-    //console.log(scy);
-    if(scy>scActive){
+    window.addEventListener("scroll", function () {
+      scy = window.document.documentElement.scrollTop;
+      // console.log(scy);
+      if (scy > scActive) {
         header.classList.add("header-active");
         logoW.style.display = "none";
         logoG.style.display = "block";
-    }else{
+      } else {
         header.classList.remove("header-active");
         logoW.style.display = "block";
         logoG.style.display = "none";
+      }
+    });
+    // 펼침 언어 기능
+    const langWord = document.querySelector(".language-word");
+    const language = document.querySelector(".language");
+    const languageLi = document.querySelector(".language li");
+    setTimeout(function () {
+      languageLi.style.transition = "all 0.2s";
+    }, 500);
+    langWord.addEventListener("click", function () {
+      language.classList.toggle("language-box-active");
+    });
+  
+    //  비디오 항목 체크 (video 태그로 파악)
+    // 모든 비디오태그 videos변수 저장
+    let videos = document.querySelectorAll(".swVisual video");
+    // console.log(videos);
+    //  비디오 시간 체크
+    // 비디오를 재생시간을 보관할 배열생성
+    let videosTimeArr = [];
+    for (let i = 0; i < videos.length; i++) {
+      // console.log(videos[i].duration);
+      // 시간을 보관한다.
+      videosTimeArr[i] = Math.ceil(videos[i].duration);
     }
-});
-
-
-// 펼침 언어 기능
-const langWord = document.querySelector(".language-word");
-const language = document.querySelector(".language");
-const languageli = document.querySelector(".language li");
-
-setTimeout(function(){
-    languageli.style.transition = "all 0.2s";
-}, 500);
-langWord.addEventListener("click", function(){
-    language.classList.toggle("language-box-active")
-});
-
-
-// 비디오 항목을 체크 (video 태그로 파악)
-let videos = document.querySelectorAll(".swVisual video");
-// 비디오 시간 체크
-let videosTimerArr = []
-for(let i = 0; i < videos.length; i++){
-    // 시간을 보관한다.
-    videosTimerArr[i] = Math.ceil(videos[i].duration) // duration: 재생하는 시간을 나타냄.
-};
-// 첫번째 비디오 자동 실행
-let videoIndex = 0;
-videos[videoIndex].play();
-// visual slide
-let swVisual = new Swiper(".swVisual", {
-    loop: true
-});
-// 슬라이드 변경 이벤트시 처리
-swVisual.on("slideChange", function(){
-    // 진행중인 비디오 멈춤
-    videos[videoIndex].pause();
-    // 다음 화면에 보이는 swiper 슬라이드 번호
-    videoIndex = swVisual.realIndex;
-    // 현재 보이는 슬라이드에 해당하는 비디오의 재생시간을 처음 설정
-    videos[videoIndex].currentTime = 0;
-    const playPromise = videos[videoIndex].play();
-    if(playPromise !== undefined){
-        playPromise.then((_)=>{}).catch((error)=> {})
-    }
-    // 방어코드
-    clearInterval(videoTimer);
-    videoReset();
-});
-// 비디오 영상이 플레이가 끝나면 다음 슬라이드로 이동
-// 늘어나는 흰색bar
-let bars = document.querySelectorAll(".bar");
-// 늘어나는 길이를 위한 값(최대 100)
-let barScaleW = 0;
-// 타이머를 생성한다.
-let videoTimer;
-function videoReset(){
-    // 처음에는 0%로 만든다.
-    barScaleW = 0;
-    // 최초에 bar을 초기화 한다.
-    for(let i = 0; i < bars.length; i++){
+    // console.log(videosTimeArr);
+    // 첫번째 비디오 자동 실행
+    let videoIndex = 0;
+    videos[videoIndex].play();
+  
+    // Visual Slide
+    // 스와퍼 슬라이드 초기화
+    let swVisual = new Swiper(".swVisual", {
+      loop: true,
+    });
+    // 슬라이드 변경 이벤트시 처리
+    swVisual.on("slideChange", function () {
+      // console.log("슬라이드 교체");
+      // 진행중인 비디오 멈춤
+      videos[videoIndex].pause();
+  
+      // 다음 화면에 보이는 swiper 슬라이드 번호
+      // console.log(swVisual.activeIndex);
+      // console.log(swVisual.realIndex);
+      videoIndex = swVisual.realIndex;
+      // 다음 비디오 재생
+      // 처음으로 비디오 플레이헤드 이동
+      // currentTime:현재 비디오 재생위치를 나타냄
+      // 이속성을 조작해서 비디오 재새위치를 변경
+      // 다음 슬라이드로 이동할 때마다 비디오를 처음부터 재생하기 위한 부분
+      videos[videoIndex].currentTime = 0;
+  
+      // https://solbel.tistory.com/1912
+      // videos[videoIndex].play();
+      const playPromise = videos[videoIndex].play();
+      if (playPromise !== undefined) {
+        playPromise.then((_) => {}).catch((error) => {});
+      }
+  
+      // 방어코드: 다음주 추가 설명
+      clearInterval(videoTimer);
+      videoReset();
+    });
+    // 비디오 영상이 플레이가 끝나면 다음 슬라이드로 이동
+    // 늘어나는 흰색 bar
+    let bars = document.querySelectorAll(".bar");
+    // console.log(bars);
+    // 늘어나는 길이를 위한 값(최대 100)
+    let barScaleW = 0;
+  
+    // 타이머를 생성한다.
+    // 비디오 타이머 초기화및 설정
+    let videoTimer;
+    function videoReset() {
+      // 처음에는 0% 로 만들려고
+      barScaleW = 0;
+      // 최초에 bar 를 초기화 한다.
+      for (let i = 0; i < bars.length; i++) {
         let tag = bars[i];
-        tag.style.width = `${barScaleW}%`
-    }
-    // 활성화 될 때 bar 클래스 선택
-    let activeBar = bars[videoIndex];
-    clearInterval(videoTimer);
-    let videotime = videosTimerArr[videoIndex];
-    videoTimer = setInterval(() => {
-        barScaleW++
+        tag.style.width = `${barScaleW}%`;
+      }
+      // 활성화 될 bar 클래스 선택
+      let activeBar = bars[videoIndex];
+      // console.log(activeBar);
+  
+      // 일단 타이머를 청소한다.
+      // setTimeout  : 1번 실행 clearTimeout()
+      // setInterval : 시간마다 연속 실행 clearInterva()
+      clearInterval(videoTimer);
+      // 비디오 플레이시간
+      // console.log("시간 배열 videosTimeArr : ", videosTimeArr);
+      // console.log("현재 번호 videoIndex : ", videoIndex);
+      let videoTime = videosTimeArr[videoIndex];
+      // console.log("선택된 시간 videoTime : ", videoTime);
+      // console.log(videoTime);
+      videoTimer = setInterval(() => {
+        barScaleW++;
+        // console.log(barScaleW);
         activeBar.style.width = `${barScaleW}%`;
-        if(barScaleW >= 100){
-            swVisual.slideNext();
-            clearInterval(videoTimer);
-            videoReset();
+  
+        if (barScaleW >= 100) {
+          swVisual.slideNext();
+          clearInterval(videoTimer);
+          videoReset();
         }
-    }, videotime * 10);
-}
-videoReset();
-const visualControlLi = document.querySelectorAll(".visual-control > li");
-visualControlLi.forEach((item, index) => {
-    item.addEventListener("click", function(){
+      }, videoTime * 10);
+    }
+    videoReset();
+  
+    // .visual-control > li 선택한다.
+    const visualControlLi = document.querySelectorAll(".visual-control > li");
+    // 클릭 이벤트를 처리하는 이벤트핸들러(약속된 함수)를 작성한다.
+    // : 이벤트(click)
+    // : 이벤트핸들러(addEventLisenter)
+    // visualControlLi[0].addEventListener("click", function(){})
+    visualControlLi.forEach((item, index) => {
+      item.addEventListener("click", function () {
+        // 클릭을 했을 때 슬라이드 번호로 점프한다.
+        console.log(index);
         videoIndex = index;
+        // Swiper 슬라이드를 직접 점프시킨다.
+        // Swiper 에 내장된 함수를 작성
+        // 슬라이드명.slidTo(번호)
         swVisual.slideTo(videoIndex);
-    })
-})
+      });
+    });
+    // 비지니스 슬라이드
+    const swBusiness = new Swiper(".swBusiness", {
+      loop: true,
+      speed: 500,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
+  };
+  
